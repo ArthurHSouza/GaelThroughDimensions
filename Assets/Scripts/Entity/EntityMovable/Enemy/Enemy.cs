a5b2c527e88577b34fd733f0778e36dddfdfcc3a
ArthurHSouza <arthurhsouza7@gmail.com>
2024-02-29 14:24:04 -0300

    chase the player

--- Assets/Scripts/Entity/EntityMovable/Enemy/Enemy.cs
+++ Assets/Scripts/Entity/EntityMovable/Enemy/Enemy.cs
@@ -8,9 +8,15 @@ public class Enemy : EntityMovable
     [Header("Patrol Points")]
     [SerializeField] protected GameObject [] patrolPoints;
     protected bool shallPatrolRight;
-    protected byte moneyDropped;
+    private byte moneyDropped;
+    [Header("Chase Information")]
+    [SerializeField] protected GameObject player;
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private float memoryChaseTimeLimit = 4f;
+    private float? memoryChaseTimeCouter = null;
+    bool shallChasePlayer;
     //protected Potion potionDropped;
-
+    
     private void Start()
     {
         onStart();
@@ -18,7 +24,8 @@ public class Enemy : EntityMovable
     override protected void onStart()
     {
         base.onStart();
-        shallPatrolRight = true;// (1 == UnityEngine.Random.Range(0, 2));
+        shallPatrolRight = (1 == UnityEngine.Random.Range(0, 2));
+        shallChasePlayer = false;
         //Only a example
         //moneyDropped = Damage * Life * DifficultyOfTheGame * Attacks.size()/2 
     }
@@ -30,22 +37,88 @@ public class Enemy : EntityMovable
     }
     private void FixedUpdate() //put all Physics related methods here
     {
+        ChaseCheck();
         Walk();
         JumpCheck();
         Jump();
         Gravity();
     }
 
-    protected override void Walk()
+    virtual protected void ChaseCheck()
     {
-        if(shallPatrolRight)
+        bool playerViewCast = Physics2D.CapsuleCast(
+            entityCollider.bounds.center, 
+            entityCollider.bounds.size, 
+            entityCollider.direction, 
+            0f, 
+            Vector2.right * Mathf.Sign(tempVelocity.x),
+            5f,
+            playerMask);
+        
+        if(shallChasePlayer && !playerViewCast)
+        {
+            if(!memoryChaseTimeCouter.HasValue)
+            {
+                memoryChaseTimeCouter = 0;
+            }
+            else if (memoryChaseTimeCouter.Value >= memoryChaseTimeLimit)
+            {
+                shallChasePlayer = false;
+            }
+            else
+            {
+                memoryChaseTimeCouter += Time.deltaTime;
+            }
+        }
+        else if (playerViewCast)
+        {
+            memoryChaseTimeCouter = null;
+            shallChasePlayer = true;
+        }
+
+        //Debug
+
+        if(memoryChaseTimeCouter.HasValue)
+        {
+            Debug.Log(memoryChaseTimeCouter);
+        }
+
+
+        Debug.DrawLine(
+         entityCollider.bounds.center,
+         new Vector3(
+             entityCollider.bounds.center.x + 5f * Mathf.Sign(tempVelocity.x),
+             entityCollider.bounds.center.y,
+             entityCollider.bounds.center.z
+             ),
+         Color.green);
+    }
+
+    override protected void Walk()
+    {
+        if (shallChasePlayer)
+            Chase();
+        else
+            Patrol();
+    }
+
+    virtual protected void Patrol()
+    {
+        if (shallPatrolRight)
         {
             tempVelocity.x = maxSpeed;
         }
         else
         {
             tempVelocity.x = -maxSpeed;
-        }    
+        }
+    }
+
+    virtual protected void Chase()
+    {
+        if (Mathf.Approximately(player.transform.position.x, rb.transform.position.x))
+            return;
+        tempVelocity.x = maxSpeed * ((player.transform.position.x > rb.transform.position.x) ? 1 : -1);
     }
 
     virtual protected void JumpCheck()
@@ -54,9 +127,9 @@ public class Enemy : EntityMovable
             new Vector2(entityCollider.bounds.center.x, entityCollider.bounds.center.y * 1.2f),
             Vector2.right * Mathf.Sign(tempVelocity.x),
             1f,
-            ~entityLayer & ~Physics2D.IgnoreRaycastLayer
+            ~entityLayer & ~playerMask & ~Physics2D.IgnoreRaycastLayer
         );
-
+        
         //Debug
         Debug.DrawLine(
             new Vector3(
@@ -74,7 +147,8 @@ public class Enemy : EntityMovable
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision == patrolPoints[0].GetComponent<Collider2D>() || collision == patrolPoints[1].GetComponent<Collider2D>())
+        if ((collision == patrolPoints[0].GetComponent<Collider2D>() && !shallPatrolRight) || 
+            collision == patrolPoints[1].GetComponent<Collider2D>() && shallPatrolRight)
         {
             shallPatrolRight = !shallPatrolRight;
         }
