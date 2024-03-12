01db52052fca6250f5a7a2fd4d2e3c0c0442c625
ArthurHSouza <arthurhsouza7@gmail.com>
2024-03-11 23:18:56 -0300

    Removed a log

--- Assets/Scripts/Entity/EntityMovable/Enemy/RunningAttack.cs
+++ Assets/Scripts/Entity/EntityMovable/Enemy/RunningAttack.cs
@@ -8,7 +8,7 @@ public class RunningAttack
         in float damageAplied, ref Vector2 velocity, in float speedUp)
     {
         if (Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer))
-            Debug.LogWarning($"Player shall recive damageAplied ({damageAplied})");
+            Debug.LogWarning($"Player shall recive damageAplied ({damageAplied}), but its not yet implemented");
         velocity.x = speedUp * Time.deltaTime;
     }
 }
